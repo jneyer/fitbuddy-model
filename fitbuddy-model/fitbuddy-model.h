4bbe89fbc01275db289b6e8914a2c9bfb4309ba8
John Neyer <jneyer@captechconsulting.com>
2016-04-23 16:21:55 -0400

    Initial Commit

---
+++ fitbuddy-model/fitbuddy-model.h
@@ -0,0 +1,19 @@
+//
+//  fitbuddy-model.h
+//  fitbuddy-model
+//
+//  Created by John Neyer on 4/23/16.
+//  Copyright © 2016 John Neyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for fitbuddy-model.
+FOUNDATION_EXPORT double fitbuddy_modelVersionNumber;
+
+//! Project version string for fitbuddy-model.
+FOUNDATION_EXPORT const unsigned char fitbuddy_modelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <fitbuddy_model/PublicHeader.h>
+
+
